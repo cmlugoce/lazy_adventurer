2627c2ae51861ecc4f0dd1a2a432167929595721
Cristina Lugo <cmlugoce@syr.edu>
2018-06-26 00:47:07 +0000

    cli file created

--- lib/country.rb
+++ lib/country.rb
@@ -3,20 +3,26 @@ require 'open-uri'
 require 'pry'
 
 
-module TravelInspiration
+module Lazy_adventurer
     class Country
 
  #1.initializemethod with @info, @url
-
+#attr name, url, summary, detail, top_sites
 
  #2.scrapeinfo
- #3.scrape season
+ #3.scrape top sites
+
+
+doc = Nokogiri::HTML(open('https://www.lonelyplanet.com/canada#articles'))
 
 
 
 
-  doc = Nokogiri::HTML(open("https://www.lonelyplanet.com/canada#articles"))
-  summary = doc.css('span.js-intro-narrative p').text
+  #doc = Nokogiri::HTML(open("url"))
+  #url = doc.css('div.SightsList-item a').attr('href').text
+  #country_page = Nokogiri::HTML(open("url"))
+
+  #@summary = doc.css('span.js-intro-narrative p').text
   binding.pry
 end
 end
