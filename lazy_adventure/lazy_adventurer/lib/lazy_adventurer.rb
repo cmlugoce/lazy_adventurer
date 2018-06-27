1f65da6631fdeae673628979b3008c471fb1b343
Cristina <38695305+cmlugoce@users.noreply.github.com>
2018-06-27 13:27:25 -0400

    Add files via upload

---
+++ lazy_adventure/lazy_adventurer/lib/lazy_adventurer.rb
@@ -0,0 +1,4 @@
+require_relative "./lazy_adventurer/version"
+require_relative "./lazy_adventurer/destination"
+require_relative "./lazy_adventurer/country"
+require_relative "./cli"
---
+++ lazy_adventure/lazy_adventurer/lib/lazy_adventurer/version.rb
@@ -0,0 +1,3 @@
+module LazyAdventurer
+  VERSION = "0.1.0"
+end
