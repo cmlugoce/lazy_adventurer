a1c5fc413af20b33c5033b32ed02a0a0e57d3a81
Cristina <38695305+cmlugoce@users.noreply.github.com>
2018-06-28 20:11:51 -0400

    Update country.rb

--- lazy_adventure/lazy_adventurer/lib/lazy_adventurer/country.rb
+++ lazy_adventure/lazy_adventurer/lib/lazy_adventurer/country.rb
@@ -10,60 +10,83 @@ module LazyAdventurer
 attr_accessor :name, :url, :details, :summary, :cities
  #1.initializemethod with @info, @url
 #attr name, url, summary, detail, top_sites
+@@all = []
 def initialize(name)
     @name = name
+    @@all << self
   end
  #2.scrapeinfo
  #3.scrape top sites
- def display_country
-   scrape_country
-   scrape_topcities
-   @summary
-   @details
+ def self.display_country
+   self.scrape_all
+   #self.scrape_top_cities
+  # self.scrape_country
+   #countries = []
+   #countries << scrape_country
+  # @summary
+   #@details
  end
 
+
+
 def self.scrape_country
-
-doc = Nokogiri::HTML(open("https://www.lonelyplanet.com/canada"))
-#  doc = Nokogiri::HTML(open("url"))
-#  url = doc.css('div#introduction p').attr('href').text
-#  country_page = Nokogiri::HTML(open("url"))
-binding.pry
-  @summary = doc.css('span.js-intro-narrative p').text
-   titles = doc.css('h2.intro-narrative__title').to_a
-
-    titles = titles.each{|title| title.text}
+doc = Nokogiri::HTML(open("https://www.lonelyplanet.com/adventure-travel/"))
+url = doc.css('div.SightsList-wrap a').attr('href')
+doc_1 = Nokogiri::HTML(open(url))
 
 
+  @summary = doc_1.css('div.intro-narrative__featured').text.gsub("\n", "").strip
+  titles = doc_1.css('h2.intro-narrative__title').to_a
+  titles = titles.collect{|title| title.text}
+  titles
 
-
- descriptions = doc.css('span.js-intro-narrative').to_a
+ descriptions = doc_1.css('p.intro-narrative__text').to_a
  descriptions = descriptions.collect{|d| d.text}
- descriptions.reject!{|item|
-                 item.downcase.end_with?("writer")}
+descriptions
 
  @details= titles.collect.with_index{|title, index|
-  "\n" + title + "\n" + descriptions[index] + "\n"
- }
-end
+"\n" + title + "\n" + descriptions[index] + "\n"
+}
+@summary
+@details
 
+end
  #need to do an array with titles and descriptions!
 
- def scrape_topcities
+ def self.scrape_top_cities
    #similartoscraping_places
-   doc = Nokogiri::HTML(open("url"))
-   url = doc.css('div.SightsList-item a').attr('href').text
-   country_page = Nokogiri::HTML(open("url"))
-
-cities = doc.css('.tlist__title').to_a
-cities = cities.each.with_index(1) do |city, index|
-
-  puts "#{index}. #{city.text}"
-end
+   doc = Nokogiri::HTML(open("https://www.lonelyplanet.com/adventure-travel/"))
+   url = doc.css('div.SightsList-wrap a').attr('href')
+   doc_2 = Nokogiri::HTML(open(url))
+   cities = doc_2.css('.tlist__title').to_a
+   cities = cities.each.with_index(1) do |city, index|
 
 end
-end
+cities
 
 end
 
- #want to have an array of countries and description
+def self.scrape_all
+ countries = []
+  doc = Nokogiri::HTML(open("https://www.lonelyplanet.com/adventure-travel/"))
+  url = doc.search('div.SightsList-wrap a').attr('href')
+  doc_2 = Nokogiri::HTML(open(url))
+   doc_2.search('div.intro-narrative__expanded').collect do |page, i|
+     new_country = LazyAdventurer::Destination.new
+     new_country.title = doc_2.search('h2.intro-narrative__title').text
+     new_country.description = doc_2.search('p.intro-narrative__text').text
+     countries << new_country
+   end
+   countries
+ end
+
+def self.all
+  @@all
+end
+
+def self.save
+  @@all
+end
+end
+
+end
