3176151a161a874d7eff000385172a8a35389f5d
Cristina <38695305+cmlugoce@users.noreply.github.com>
2018-06-28 20:13:23 -0400

    added the cli file

--- lazy_adventure/lazy_adventurer/lib/lazy_adventurer.rb
+++ lazy_adventure/lazy_adventurer/lib/lazy_adventurer.rb
@@ -1,4 +1,4 @@
 require_relative "./lazy_adventurer/version"
 require_relative "./lazy_adventurer/destination"
 require_relative "./lazy_adventurer/country"
-require_relative "./cli"
+require_relative "./lazy_adventurer/cli"
