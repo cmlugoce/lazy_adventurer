30d3f869955879490d4b8ee271b11d2d0d7330e5
Cristina <38695305+cmlugoce@users.noreply.github.com>
2018-06-29 00:02:39 -0400

    Update cli.rb

--- lazy_adventure/lazy_adventurer/lib/lazy_adventurer/cli.rb
+++ lazy_adventure/lazy_adventurer/lib/lazy_adventurer/cli.rb
@@ -1,3 +1,6 @@
+equire_relative 'destinations.rb'
+require_relative 'country.rb'
+
 module LazyAdventurer
  class CLI
 
@@ -9,31 +12,35 @@ end
 
 def list_destination
 
-  @places = LazyAdventurer::Destination.place
+  @places = LazyAdventurer::DestinationScraper.place
   puts @places.collect.with_index{|place, index|
  "\t#{index + 1}. #{place.name}. #{place.region}"}
 end
 
-def menu
+def menu(places)
   puts "Check out the countries and their top sites. Type 'ciao' to leave the program or type 'list' to return the menu"
   input = nil
   while input != "ciao"
   input = gets.strip.downcase
-  case input
-  when "1"
-    puts 'cool'
-  when "2"
-    puts 'yay'
-  when "list"
-    list_destination
-  when 'ciao'
-    goodbye
-  else
-    puts "Invalid input. Please try again"
 
-  end
-  end
+   if input.to_i > 0 && input.to_i < 7
+     selection =  @places[input.to_i-1].name
+    puts "Great choice. #{selection} is a beautiful country!"
+    country_details(selection)
+  elsif input == 'list'
+    list_destination
+  else
+    puts "Invalid input, please try again"
 end
+end
+end
+
+def country_details(selection)
+  country = LazyAdventurer::Country.new(selection)
+  puts Country.display_country
+end
+
+
 def goodbye
   puts 'hasta la vista'
 end
